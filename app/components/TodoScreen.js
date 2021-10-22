86c0baa1a380e9e0db180fd5571c42bfa5433fc7
VuNgN <vunguyen220901@gmail.com>
2021-10-22 22:15:01 +0700

    Update: empty screen

--- app/components/TodoScreen.js
+++ app/components/TodoScreen.js
@@ -28,6 +28,9 @@ const TodoScreen = ({
   const [incompleteNum, changeIncompleteNum] = useState(incomplete.length);
   const [completedNum, changeCompletedNum] = useState(completed.length);
   const [modalVisible, setModalVisible] = useState(false);
+  const isIncomplete = incomplete.length > 0;
+  const isCompleted = completed.length > 0;
+  const isIncompleteAndCompleted = isIncomplete || isCompleted;
 
   useEffect(() => {
     changeDate(new Date().getDate());
@@ -63,80 +66,105 @@ const TodoScreen = ({
             {incompleteNum} incomplete, {completedNum} completed
           </Text>
         </View>
-        <ScrollView style={styles.scrollView}>
-          <View style={styles.todoWrapper}>
-            <Text style={styles.H2Title}>Incomplete</Text>
-            {incomplete.map(todo => (
-              <View style={styles.incomplete} key={todo.id}>
-                <BouncyCheckbox
-                  size={24}
-                  fillColor={colors.light}
-                  unfillColor={colors.light}
-                  disableText
-                  iconStyle={{
-                    borderColor: colors.whiteText,
-                    borderRadius: 6,
-                    borderWidth: 2,
-                  }}
-                  isChecked={todo.isCompleted}
-                  disableBuiltInState
-                  onPress={() => completeTodo(todo.id)}
-                />
-                <View style={styles.incompleteContentWrapper}>
-                  <Text style={styles.incompleteContent}>{todo.content}</Text>
-                  <Text style={styles.incompleteType}>{todo.type}</Text>
-                </View>
-                <Pressable
-                  onPress={() => onPressDoneBtn(todo.id)}
-                  style={styles.doneButton}>
-                  <Text style={styles.doneButtonText}>done</Text>
-                </Pressable>
+        {isIncompleteAndCompleted ? (
+          <ScrollView style={styles.scrollView}>
+            {isIncomplete && (
+              <View style={styles.todoWrapper}>
+                <Text style={styles.H2Title}>Incomplete</Text>
+                {incomplete.map(todo => (
+                  <Pressable
+                    style={styles.incomplete}
+                    key={todo.id}
+                    onPress={() => completeTodo(todo.id)}>
+                    <BouncyCheckbox
+                      size={24}
+                      fillColor={colors.light}
+                      unfillColor={colors.light}
+                      disableText
+                      iconStyle={{
+                        borderColor: colors.whiteText,
+                        borderRadius: 6,
+                        borderWidth: 2,
+                      }}
+                      isChecked={todo.isCompleted}
+                      disableBuiltInState
+                      onPress={() => completeTodo(todo.id)}
+                    />
+                    <View style={styles.incompleteContentWrapper}>
+                      <Text style={styles.incompleteContent} numberOfLines={1}>
+                        {todo.content}
+                      </Text>
+                      <Text style={styles.incompleteType} numberOfLines={1}>
+                        {todo.type}
+                      </Text>
+                    </View>
+                    <Pressable
+                      onPress={() => onPressDoneBtn(todo.id)}
+                      style={styles.doneButton}>
+                      <Text style={styles.doneButtonText}>done</Text>
+                    </Pressable>
+                  </Pressable>
+                ))}
               </View>
-            ))}
-          </View>
-          <View style={styles.todoWrapper}>
-            <Text style={styles.H2Title}>Completed</Text>
-            {completed.map(todo => (
-              <View style={styles.completed} key={todo.id}>
-                <BouncyCheckbox
-                  size={24}
-                  fillColor={colors.whiteText}
-                  unfillColor={colors.light}
-                  disableText
-                  iconStyle={{
-                    borderColor: colors.whiteText,
-                    borderRadius: 6,
-                    borderWidth: 2,
-                  }}
-                  isChecked={todo.isCompleted}
-                  disableBuiltInState
-                  onPress={() => completeTodo(todo.id)}
-                />
-                <View style={styles.completedContentWrapper}>
-                  <Text style={styles.completedContent}>{todo.content}</Text>
-                </View>
-                <Pressable
-                  onPress={() => onPressDoneBtn(todo.id)}
-                  style={[
-                    styles.doneButton,
-                    {
-                      borderColor: colors.buttonBorderColor,
-                    },
-                  ]}>
-                  <Text
-                    style={[
-                      styles.doneButtonText,
-                      {
-                        color: colors.buttonBackgroundColor,
-                      },
-                    ]}>
-                    done
-                  </Text>
-                </Pressable>
+            )}
+            {isCompleted && (
+              <View style={styles.todoWrapper}>
+                <Text style={styles.H2Title}>Completed</Text>
+                {completed.map(todo => (
+                  <Pressable
+                    style={styles.completed}
+                    key={todo.id}
+                    onPress={() => completeTodo(todo.id)}>
+                    <BouncyCheckbox
+                      size={24}
+                      fillColor={colors.whiteText}
+                      unfillColor={colors.light}
+                      disableText
+                      iconStyle={{
+                        borderColor: colors.whiteText,
+                        borderRadius: 6,
+                        borderWidth: 2,
+                      }}
+                      isChecked={todo.isCompleted}
+                      disableBuiltInState
+                      onPress={() => completeTodo(todo.id)}
+                    />
+                    <View style={styles.completedContentWrapper}>
+                      <Text style={styles.completedContent} numberOfLines={2}>
+                        {todo.content}
+                      </Text>
+                    </View>
+                    <Pressable
+                      onPress={() => onPressDoneBtn(todo.id)}
+                      style={[
+                        styles.doneButton,
+                        {
+                          borderColor: colors.buttonBorderColor,
+                        },
+                      ]}>
+                      <Text
+                        style={[
+                          styles.doneButtonText,
+                          {
+                            color: colors.buttonBackgroundColor,
+                          },
+                        ]}>
+                        done
+                      </Text>
+                    </Pressable>
+                  </Pressable>
+                ))}
               </View>
-            ))}
+            )}
+          </ScrollView>
+        ) : (
+          <View style={styles.emptyTodoSwrapper}>
+            <Text style={styles.emptyTodoText}>“To-do list : to-do list!”</Text>
+            <Text style={styles.emptyTodoTextDesc}>
+              Click the '+' button to change your life.
+            </Text>
           </View>
-        </ScrollView>
+        )}
         <AddTodoModal
           modalVisible={modalVisible}
           setModalVisible={setModalVisible}
@@ -209,12 +237,14 @@ const styles = StyleSheet.create({
     alignItems: 'flex-start',
   },
   incompleteContentWrapper: {
-    marginLeft: 16,
+    marginHorizontal: 16,
+    flex: 1,
   },
   incompleteContent: {
     fontFamily: 'Inter-Medium',
     fontSize: 18,
     color: colors.blackText,
+    overflow: 'hidden',
   },
   incompleteType: {
     fontFamily: 'Inter-SemiBold',
@@ -227,7 +257,8 @@ const styles = StyleSheet.create({
     alignItems: 'flex-start',
   },
   completedContentWrapper: {
-    marginLeft: 16,
+    marginHorizontal: 16,
+    flex: 1,
   },
   completedContent: {
     fontFamily: 'Inter-Medium',
@@ -249,5 +280,21 @@ const styles = StyleSheet.create({
     fontFamily: 'Inter-Medium',
     fontSize: 18,
     color: colors.grayTextLight,
+  },
+  emptyTodoSwrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  emptyTodoText: {
+    fontSize: 24,
+    fontFamily: 'Inter-Bold',
+    color: colors.titleText,
+  },
+  emptyTodoTextDesc: {
+    fontSize: 18,
+    fontFamily: 'Inter-Medium',
+    color: colors.grayTextLight,
+    marginTop: 5,
   },
 });
